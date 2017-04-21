14ee5791ad90fb47258e8e1c298e02c19b9c2fc6
Giang-PC\Giang <duygiangdg@gmail.com>
2017-04-22 00:49:48 +0700

    starter

---
+++ QLBanHang/Model/NhanVienMod.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLBanHang.Model
+{
+    class NhanVienMod
+    {
+        ConnectToSQL con = new ConnectToSQL();
+        SqlCommand cmd = new SqlCommand();
+
+        public DataTable GetData()
+        {
+            DataTable dt = new DataTable();
+
+            cmd.CommandText = "select * from tb_NhanVien";
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = con.Connection;
+
+            try
+            {
+                con.OpenConn();
+                SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                sda.Fill(dt);
+                con.CloseConn();
+            }
+            catch (Exception ex)
+            {
+                string mes = ex.Message;
+                cmd.Dispose();
+                con.CloseConn();
+            }
+
+            return dt;
+        }
+
+        public bool AddData(NhanVienObj nvObj)
+        {
+            cmd.CommandText = "insert into tb_NhanVien values ('" 
+                + nvObj.Ma + "','"
+                + nvObj.Ten + "','"
+                + nvObj.GioiTinh + "', CONVER(DATE,'"
+                + nvObj.NamSinh.ToShortDateString() + "',103),'" 
+                + nvObj.DiaChi + "','"
+                + nvObj.Sdt + "','"
+                + nvObj.MatKhau + "')";
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = con.Connection;
+
+            try
+            {
+                con.OpenConn();
+                cmd.ExecuteNonQuery();
+                con.CloseConn();
+            }
+            catch (Exception ex)
+            {
+                string mes = ex.Message;
+                cmd.Dispose();
+                con.CloseConn();
+            }
+            return false;
+        }
+    }
+}
---
+++ QLBanHang/Model/NhanVienObj.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLBanHang.Model
+{
+    class NhanVienObj
+    {
+        string ma, ten, gioiTinh, diaChi, sdt, matKhau;
+        DateTime namSinh;
+
+        public string DiaChi
+        {
+            get
+            {
+                return diaChi;
+            }
+
+            set
+            {
+                diaChi = value;
+            }
+        }
+
+        public string GioiTinh
+        {
+            get
+            {
+                return gioiTinh;
+            }
+
+            set
+            {
+                gioiTinh = value;
+            }
+        }
+
+        public string Ma
+        {
+            get
+            {
+                return ma;
+            }
+
+            set
+            {
+                ma = value;
+            }
+        }
+
+        public string MatKhau
+        {
+            get
+            {
+                return matKhau;
+            }
+
+            set
+            {
+                matKhau = value;
+            }
+        }
+
+        public DateTime NamSinh
+        {
+            get
+            {
+                return namSinh;
+            }
+
+            set
+            {
+                namSinh = value;
+            }
+        }
+
+        public string Sdt
+        {
+            get
+            {
+                return sdt;
+            }
+
+            set
+            {
+                sdt = value;
+            }
+        }
+
+        public string Ten
+        {
+            get
+            {
+                return ten;
+            }
+
+            set
+            {
+                ten = value;
+            }
+        }
+
+        public NhanVienObj() { }
+        public NhanVienObj(string ma, string ten, string gioiTinh, DateTime namSinh, string diaChi, string sdt, string matKhau)
+        {
+            this.ma = ma;
+            this.ten = ten;
+            this.gioiTinh = gioiTinh;
+            this.namSinh = namSinh;
+            this.diaChi = diaChi;
+            this.sdt = sdt;
+            this.matKhau = matKhau;
+        }
+    }
+}
